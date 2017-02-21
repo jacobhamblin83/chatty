1c1a6d0711f6cc4d16fff6b89d0557d2c5b56ecd
Jacob Hamblin <jacobhamblin83@gmail.com>
2017-02-21 15:16:56 -0700

    fixed bug

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+var express = require('express');
+var bodyParser = (require('body-parser'));
 
+var app = express();
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+var messages = [];
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+    messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+  console.log(req.body);
+});
+
+
+app.listen(3000);
